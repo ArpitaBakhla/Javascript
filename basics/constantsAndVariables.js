768b804ed471dc0cab07d00b0f14393afd146bc5
ArpitaBakhla <230815184+ArpitaBakhla@users.noreply.github.com>
2026-06-17 13:13:01 +0000

    Constants/Variables

---
+++ basics/constantsAndVariables.js
@@ -0,0 +1,8 @@
+const accountid="123"
+let email="abc@gmail.com"
+var address="abc"
+let accountstate;
+
+console.table([accountid,email,address,accountstate])
+
+/*don't use var to to avoid block and functional scope */ 
