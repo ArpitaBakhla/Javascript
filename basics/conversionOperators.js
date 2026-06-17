884d908aa0495795ca696a0fde8c187ce509c427
ArpitaBakhla <230815184+ArpitaBakhla@users.noreply.github.com>
2026-06-17 14:07:04 +0000

    ConversionOperators

---
+++ basics/conversionOperators.js
@@ -0,0 +1,97 @@
+let score=33 
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueOfNumber=Number(score)
+console.log(typeof valueOfNumber);
+console.log(valueOfNumber);   //33
+
+
+
+
+let score1="33abc"
+
+console.log(typeof score1);
+console.log(typeof (score1));
+
+let valueOfNumber1=Number(score1)
+console.log(typeof valueOfNumber1);
+console.log(valueOfNumber1);  //NaN
+
+
+
+
+let score2=null
+
+console.log(typeof score2);
+console.log(typeof (score2));
+
+let valueOfNumber2=Number(score2)
+console.log(typeof valueOfNumber2);
+console.log(valueOfNumber2);  //0
+
+
+
+
+let score3=undefined
+
+console.log(typeof score3);
+console.log(typeof (score3));
+
+let valueOfNumber3=Number(score3)
+console.log(typeof valueOfNumber3);
+console.log(valueOfNumber3);  //NaN
+
+
+
+let isLogged=1
+
+console.log(typeof isLogged);
+console.log(typeof (isLogged));
+
+let booleanIsLogged=Boolean(isLogged)
+console.log(typeof booleanIsLogged);
+console.log(booleanIsLogged);  //true
+
+
+
+
+let isLogged1=""
+
+console.log(typeof isLogged1);
+console.log(typeof (isLogged1));
+
+let booleanIsLogged1=Boolean(isLogged1)
+console.log(typeof booleanIsLogged1);
+console.log(booleanIsLogged1);   //false
+
+
+
+
+let isLogged2="abc"
+
+console.log(typeof isLogged2);
+console.log(typeof (isLogged2));
+
+let booleanIsLogged2=Boolean(isLogged2)
+console.log(typeof booleanIsLogged2);
+console.log(booleanIsLogged2);  //true
+
+
+
+
+let string="abc"
+
+let isString=String(string)
+console.log(typeof isString);
+console.log(isString); 
+
+
+
+
+let string1=13
+
+let isString1=String(string1)
+console.log(typeof isString1);
+console.log(isString1); 
