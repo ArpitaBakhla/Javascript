e7d5e0d1875b28c67b1d365e8bc3ad1fe3bf172f
ArpitaBakhla <230815184+ArpitaBakhla@users.noreply.github.com>
2026-06-17 13:20:45 +0000

    Datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)  //we are using nodejs,not browser
+
+
+let name="Arpita"
+let id=12 //bigint can also be used for bigger values
+let boolean= true
+let address; //undefined
+let value=null //is a standalone value
+//symbol=>unique
+
+console.log(typeof "Arpita");
+console.log(typeof 2);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
