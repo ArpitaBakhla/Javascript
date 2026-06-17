5707f8724e205f860cd276406d1510e01e1cea28
ArpitaBakhla <230815184+ArpitaBakhla@users.noreply.github.com>
2026-06-17 16:48:41 +0000

    String outputs

---
+++ basics/stringToNumber.js
@@ -0,0 +1,21 @@
+let str1="Arpita"
+let str2="Bakhla"
+let str3=str1+str2
+
+console.log(str3);
+
+console.log(1+2);
+console.log("1"+"2");
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+2+"2");
+console.log("1"+2+2);
+
+//console.log((((3*4)+7)%8-9);   use parenthesis
+
+console.log(+true);
+console.log(+"");
+
+
+
+
