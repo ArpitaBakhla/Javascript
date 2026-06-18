2fbf96d7efc3a79e890623bd36665ea9142ef3c7
ArpitaBakhla <230815184+ArpitaBakhla@users.noreply.github.com>
2026-06-18 15:05:39 +0000

    Stack and heap

---
+++ BASICS/stackAndHeap.js
@@ -0,0 +1,23 @@
+// Stack:Primitive  Heap:Non-Primitive
+
+let value="day"
+let value1=value
+value1="night"
+
+console.log(value);
+console.log(value1);
+
+
+let user={
+    name:"Arpita",
+    upi:"user@ybl"
+}
+
+let user1=user
+user1.name="Supriya"
+
+console.log(user.name);
+console.log(user1.name);
+
+
+
