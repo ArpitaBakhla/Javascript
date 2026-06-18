bfc04df6df0a72c415945ce43d3b8aed4a99742e
ArpitaBakhla <230815184+ArpitaBakhla@users.noreply.github.com>
2026-06-18 15:06:04 +0000

    String

---
+++ BASICS/strings.js
@@ -0,0 +1,28 @@
+const name="arpita";
+const repo=2;
+
+console.log(`Hello my name is ${name} and my repo count is ${repo}`);
+
+
+
+const gamename= new String ('supriya')
+
+console.log(gamename[0]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(2));
+console.log(gamename.indexOf('y'));
+console.log(gamename.substring(0,4));
+console.log(gamename.slice(-6,4)); //can give negative values
+
+const value="   arpita   ";
+console.log(value);
+console.log(value.trim());
+
+const url="https://google.com/arpita%ss"
+console.log(url.replace('%','-'));
+console.log(url.includes('ss'));
+console.log(url.split('a'));
+
+
